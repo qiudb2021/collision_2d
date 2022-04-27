4fddac360adb9dd6b9737b62268b5f71c46f13ca
qiudb2021 <qiudb2022@outlook.com>
2022-04-27 11:24:54 +0800

    四叉树

---
+++ test.js
@@ -0,0 +1,30 @@
+function testDiv() {
+    for (let i = 0; i < 10; i++) {
+        let result;
+        let count = 1000*1000*10000
+        console.time("div")
+        let floor = Math.floor
+        while(count--) {
+            // result = parseInt(11 / 2);
+            result = floor(11/2)
+            // result = 11 / 2;
+
+        }
+        console.timeEnd("div")
+    }
+}
+
+testDiv(1000)
+
+function testBit() {
+    for (let i = 0; i < 10; i++) {
+        let result;
+        let count = 1000*1000*10000
+        console.time("bit")
+        while(count--) {
+            result = 11 >> 1;
+        }
+        console.timeEnd("bit")
+    }
+}
+// testBit()
